f17984e7c0b77d2226e8d54b35cd7a7479b8d0d9
Landon Turner <lturner@LTurner-MBP.lan1>
2020-02-25 18:15:26 -0500

    params work now

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -6,8 +6,9 @@ import MovieCard from './MovieCard'
 const Movie = (props) => {
   const [movie, setMovie] = useState();
   console.log(props);
+  const {id} = useParams();
   useEffect(() => {
-    const id = props.match.params.id;
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -22,7 +23,7 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
+  },[id]);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
